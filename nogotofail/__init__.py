2e27cb074b3b0f4200a8a2fc36887f21ab361490
Chad Brubaker <cbrubaker@google.com>
2014-11-11 15:08:29 -0800

    Version bump to 1.0.0
    Now is as good a point as any to mark a 1.0.0 'release' and branch off a
    dev branch so we keep master stable.

--- nogotofail/__init__.py
+++ nogotofail/__init__.py
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 
-version_info = (0, 1, 0)
+version_info = (1, 0, 0)
 __version__ = ".".join([str(v) for v in version_info])
