28da7176c3eeec611e7e1e3ac9d85b93a0f31b42
Alex Klyubin <klyubin@google.com>
2014-12-14 17:37:23 -0800

    Human-readable TLS alert representation

--- nogotofail/mitm/util/tls/types/alert.py
+++ nogotofail/mitm/util/tls/types/alert.py
@@ -15,6 +15,39 @@ limitations under the License.
 '''
 import struct
 
+level_names = {
+    1: "warning",
+    2: "fatal",
+}
+
+description_names = {
+    0:  "close_notify",
+    10:  "unexpected_message",
+    20:  "bad_record_mac",
+    21:  "decryption_failed",
+    22:  "record_overflow",
+    30:  "decompression_failure",
+    40:  "handshake_failure",
+    41:  "no_certificate",
+    42:  "bad_certificate",
+    43:  "unsupported_certificate",
+    44:  "certificate_revoked",
+    45:  "certificate_expired",
+    46:  "certificate_unknown",
+    47:  "illegal_parameter",
+    48:  "unknown_ca",
+    49:  "access_denied",
+    50:  "decode_error",
+    51:  "decrypt_error",
+    60:  "export_restriction",
+    70:  "protocol_version",
+    71:  "insufficient_security",
+    80:  "internal_error",
+    86:  "inappropriate_fallback",
+    90:  "user_canceled",
+    100: "no_renegotiation",
+    110: "unsupported_extension",
+}
 
 class Alert(object):
     LEVEL_FATAL = 2
@@ -33,3 +66,9 @@ class Alert(object):
 
     def to_bytes(self):
         return struct.pack("BB", self.level, self.description)
+
+    def __str__(self):
+        return ("TLS %s alert %s (%d)"
+            % (level_names.get(self.level),
+               description_names.get(self.description), self.description))
+
