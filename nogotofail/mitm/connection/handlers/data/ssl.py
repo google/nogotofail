75701bbfd810d63e0dd95bc2931168a90fa110fb
yzninja <yzninja@gmail.com>
2015-07-24 22:33:04 +0800

    Added new data handler "noforwardsecrecy".
    The "noforwardsecrecy" data handler detects cipher suites negotiated between client and server (in the Server Hello message) which don't support forward secrecy i.e. Ephemeral Diffie-Hellman methods DHE or ECDHE.

--- nogotofail/mitm/connection/handlers/data/ssl.py
+++ nogotofail/mitm/connection/handlers/data/ssl.py
@@ -19,7 +19,10 @@ from nogotofail.mitm.connection.handlers.data import handlers
 from nogotofail.mitm.connection.handlers.data import DataHandler
 from nogotofail.mitm.connection.handlers.store import handler
 from nogotofail.mitm.event import connection
+from nogotofail.mitm import util
 from nogotofail.mitm.util import ssl2, tls, vuln
+from nogotofail.mitm.util.tls.types import HandshakeMessage
+
 
 class _TlsRecordHandler(DataHandler):
     """Base class for a handler that acts on TlsRecords in a Tls connection.
@@ -131,7 +134,7 @@ class InsecureCipherDetectionHandler(DataHandler):
                 (", ".join(null_ciphers)))
 
         # Check for NULL integrity ciphers
-        integ_ciphers =  [str(c) for c in client_hello.ciphers if str(c).endswith("_NULL")]
+        integ_ciphers = [str(c) for c in client_hello.ciphers if str(c).endswith("_NULL")]
         if integ_ciphers:
             self._handle_bad_ciphers(integ_ciphers,
                 "Client enabled NULL integrity TLS/SSL cipher suites %s" %
@@ -172,3 +175,46 @@ class WeakTLSVersionDetectionHandler(DataHandler):
                 self.log(logging.ERROR,
                         "Client enabled SSLv3 protocol without TLS_FALLBACK_SCSV")
             self.log_attack_event(data="SSLv3")
+
+
+@handler.passive(handlers)
+class NoForwardSecrecy(_TlsRecordHandler):
+    name = "noforwardsecrecy"
+    description = (
+        "Detects selected server cipher suites which don't support "
+        "Diffie-Hellman key exchange (DHE or ECDHE) i.e. in SERVER_HELLO "
+        "response")
+
+    def on_tls_response(self, record):
+        try:
+            for i, message in enumerate(record.messages):
+                # Check for Server Hello message
+                if (isinstance(message, tls.types.HandshakeMessage) and
+                        message.type == HandshakeMessage.TYPE.SERVER_HELLO):
+                    server_hello = message.obj
+                    selected_cipher = str(server_hello.cipher)
+                    _connection = self.connection
+                    destination = _connection.hostname if \
+                        _connection.hostname else _connection.server_addr
+                    debug_message = ["Selected cipher \"", selected_cipher,
+                            "\" for connection to \"", destination, "\""]
+                    self.log(logging.DEBUG, "".join(debug_message))
+                    """ Check if Ephemeral Diffie-Hellman key exchange is
+                        used in selected cipher """
+                    fs_key_strings = ["DHE", "ECDHE"]
+                    if not [fs_string for fs_string in fs_key_strings
+                            if fs_string in selected_cipher]:
+                        error_message = \
+                            ["Cipher suite key exhange technqiue doesn't ",
+                             "support forward secrecy. ",
+                             "Cipher suite - [", selected_cipher, "]"]
+                        self.log(logging.INFO, "".join(error_message))
+                        self.log_event(logging.INFO,
+                            connection.AttackEvent(
+                                self.connection, self.name, True, ""))
+                        self.connection.vuln_notify(
+                            util.vuln.VULN_NO_FORWARD_SECRECY)
+        except AttributeError:
+            # Where TLS record contains no messages ignore exception raised.
+            pass
+        return record.to_bytes()
